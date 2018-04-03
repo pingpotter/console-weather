7af2e9b85e9322d1df5fba76d99868b85fe45b77
pingpotter <pingpotter@live.com>
2018-04-03 09:48:02 +0700

    Clearing code

--- console-weather.go
+++ console-weather.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"strconv"
-
-	owm "github.com/briandowns/openweathermap"
 )
-
-type Config struct {
-	Proxy  string `json:"Proxy"`
-	APIKey string `json:"APIKey"`
-}
 
 func main() {
 
@@ -21,68 +11,11 @@ func main() {
 	fmt.Println(weatherCelsius(34, "Tak many"))
 	fmt.Println(weatherCelsius(17, "Phuket rainy"))
 	fmt.Println(weatherCelsius(9, "Chiang-mai cold"))
-
-	fmt.Println(weatherCelsius2(0, "Locations"))
-	fmt.Println(weatherCelsius2(1234567890.239, "Locations2"))
-	fmt.Println(weatherCelsius2(-1234567890.234, "Locations3"))
-
-	//API by open weather map
-
-	//Read configuration file
-
-	config := LoadConfiguration("conf.json")
-	//os.Setenv("HTTP_PROXY", config.Proxy)
-
-	w, err := owm.NewCurrent("C", "EN", config.APIKey)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	w.CurrentByID(1609350) //Bangkok
-	fmt.Println(weatherCelsius2(w.Main.Temp, fmt.Sprintf("%s,%s is %s", w.Name, w.Sys.Country, w.Weather[0].Description)))
-
-	w.CurrentByID(1608528) //Changwat Nakhon Ratchasima
-	fmt.Println(weatherCelsius2(w.Main.Temp, fmt.Sprintf("%s,%s is %s", w.Name, w.Sys.Country, w.Weather[0].Description)))
-
-	w.CurrentByID(524901) //Moscow
-	fmt.Println(weatherCelsius2(w.Main.Temp, fmt.Sprintf("%s,%s is %s", w.Name, w.Sys.Country, w.Weather[0].Description)))
-}
-
-func LoadConfiguration(file string) Config {
-	var config Config
-	configFile, err := os.Open(file)
-	defer configFile.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
-	return config
 }
 
 func weatherCelsius(celsius int, desc string) string {
 
-	var ret string
-
-	switch celsius {
-	case 25:
-		ret = " _  _" + "\n" + " _||_ " + "\n" + "|_  _| c" + "\n" + desc
-	case 34:
-		ret = " _   " + "\n" + " _||_|" + "\n" + " _|  | c" + "\n" + desc
-	case 17:
-		ret = "    _" + "\n" + "  |  |" + "\n" + "  |  | c" + "\n" + desc
-	case 9:
-		ret = " _   " + "\n" + "|_|   " + "\n" + " _| c" + "\n" + desc
-	default:
-		ret = " _   " + "\n" + "|_|   " + "\n" + " |_| c" + "\n" + desc
-
-	}
-	return ret
-}
-
-func weatherCelsius2(celsius float64, desc string) string {
-
-	strCel := strconv.FormatFloat(celsius, 'f', 2, 64)
+	strCel := strconv.Itoa(celsius)
 
 	var str [][]string
 
@@ -96,7 +29,6 @@ func weatherCelsius2(celsius float64, desc string) string {
 	ret += desc
 
 	return ret
-
 }
 
 func printOut(digit [][]string) string {
